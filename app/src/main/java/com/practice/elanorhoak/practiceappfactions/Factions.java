20c8947ca5e78d1f56593458212011c8db85f631
ehoak <elho0647@colorado.edu>
2017-07-30 17:28:42 -0600

    Adding toast for bt.

--- app/src/main/java/com/practice/elanorhoak/practiceappfactions/Factions.java
+++ app/src/main/java/com/practice/elanorhoak/practiceappfactions/Factions.java
@@ -1,5 +1,6 @@
 package com.practice.elanorhoak.practiceappfactions;
 
+import android.bluetooth.BluetoothAdapter;
 import android.graphics.drawable.AnimationDrawable;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
@@ -8,15 +9,20 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 public class Factions extends AppCompatActivity {
 
+    //create image view for main animal display
     ImageView image;
-    //AnimationDrawable animationView = new AnimationDrawable();
-    //Handler handler = new Handler();
-    boolean runLlama, runMallard, runHare;
-    int i = 0;
 
+    //AnimationDrawable animationView = new AnimationDrawable(); //used for animated backgrounds
+    //Handler handler = new Handler();
+
+    boolean runLlama, runMallard, runHare;      //select which animal to display
+    int i = 0;                                  //set iteration picture
+
+    //create picture arrays to loop through
     int[] llamaArray = {R.mipmap.l_g1a, R.mipmap.l_g2, R.mipmap.l_g3,
             R.mipmap.l_g4, R.mipmap.l_g3, R.mipmap.l_g2};
     int[] mallardArray = {R.mipmap.m_g1, R.mipmap.m_g2, R.mipmap.m_g3,
@@ -24,10 +30,17 @@ public class Factions extends AppCompatActivity {
     int[] hareArray = {R.mipmap.h_g1, R.mipmap.h_g2, R.mipmap.h_g3,
             R.mipmap.h_g4, R.mipmap.h_g3, R.mipmap.h_g2};
 
+    //set up Bluetooth adapter
+    BluetoothAdapter mBluetoothAdapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_factions);
+
+        mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        checkBluetooth();
 
         runLlama    = false;
         runMallard  = false;
@@ -37,6 +50,17 @@ public class Factions extends AppCompatActivity {
 
         image.removeCallbacks(runnable);
         image.postDelayed(runnable, 100);
+
+    }
+
+    //check if device supports Bluetooth
+    public void checkBluetooth(){
+        if(mBluetoothAdapter == null){
+            Toast.makeText(getApplicationContext(),"This device does not support Bluetooth",Toast.LENGTH_SHORT).show();
+        }
+        else{
+            Toast.makeText(getApplicationContext(),"Tada! Bluetooth works!",Toast.LENGTH_SHORT).show();
+        }
 
     }
 
